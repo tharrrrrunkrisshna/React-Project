67f7f07ebc693a3d350577262c5155e23d7adefa
tharrrrrunkrisshna <43927016+tharrrrrunkrisshna@users.noreply.github.com>
2023-04-05 15:38:22 +0530

    Create ImageCompressor.js

---
+++ Image Compressor/components/ImageCompressor.js
@@ -0,0 +1,137 @@
+import React from "react";
+import Img2 from "../download image.jpg";
+import imageCompression from "browser-image-compression";
+import '../Navbar.css'
+
+import Card from "react-bootstrap/Card";
+
+export default class imageCompressor extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      compressedLink:`${Img2}`,
+      originalImage: "",
+      originalLink: "",
+      clicked: false,
+      uploadImage: false
+    };
+  }
+
+  handle = e => {
+    const imageFile = e.target.files[0];
+    this.setState({
+      originalLink: URL.createObjectURL(imageFile),
+      originalImage: imageFile,
+      outputFileName: imageFile.name,
+      uploadImage: true
+    });
+  };
+
+  changeValue = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  click = e => {
+    e.preventDefault();
+
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 500,
+      useWebWorker: true
+    };
+
+    if (options.maxSizeMB >= this.state.originalImage.size / 1024) {
+      alert("Image is too small, can't be Compressed!");
+      return 0;
+    }
+
+    let output;
+    imageCompression(this.state.originalImage, options).then(x => {
+      output = x;
+
+      const downloadLink = URL.createObjectURL(output);
+      this.setState({
+        compressedLink: downloadLink
+      });
+    });
+
+    this.setState({ clicked: true });
+    return 1;
+  };
+
+  render() {
+    return (
+      <div >
+        
+        <div className="steps">
+            <center style={{color:'red'}}>
+          <h1>Steps to follow</h1>
+          <h3>Step1➡️  Upload Image</h3>
+          <h3>Step2➡️ Click on Compress</h3>
+          <h3>Step3➡️ Download Compressed Image</h3>
+          </center>
+          <br></br>
+        </div>
+
+        <div className="row mt-5">
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+            {this.state.uploadImage ? (
+              <Card.Img
+                className="ht"
+                variant="top"
+                src={this.state.originalLink}
+              ></Card.Img>
+            ) : (
+              <Card.Img
+                className="ht"
+                variant="top"
+                src="https://www.lifewire.com/thmb/TRGYpWa4KzxUt1Fkgr3FqjOd6VQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cloud-upload-a30f385a928e44e199a62210d578375a.jpg"
+              ></Card.Img>
+            )}
+            <div className="d-flex justify-content-center">
+              <input
+                type="file"
+                accept="image/*"
+                className="mt-2 btn btn-dark w-75"
+                onChange={e => this.handle(e)}
+              />
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-12 mb-5 mt-5 col-sm-12 d-flex justify-content-center align-items-baseline">
+            <center>
+            <br />
+            {this.state.outputFileName ? (
+              <button
+                type="button"
+                className=" btn btn-dark"
+                onClick={e => this.click(e)}
+              >
+                Compress
+              </button>
+            ) : (
+              <></>
+            )}
+            </center>
+          </div>
+
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 mt-3">
+            <Card.Img variant="top" src={this.state.compressedLink}></Card.Img>
+            {this.state.clicked ? (
+              <div className="d-flex justify-content-center">
+                <a
+                  href={this.state.compressedLink}
+                  download={this.state.outputFileName}
+                  className="mt-2 btn btn-dark w-75"
+                >
+                  Download
+                </a>
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
