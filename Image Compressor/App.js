8c7f51bbe8437d437a0de89d85bf0354a8edd934
tharrrrrunkrisshna <43927016+tharrrrrunkrisshna@users.noreply.github.com>
2023-04-05 15:34:16 +0530

    Create App.js

---
+++ Image Compressor/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./Footer";
+import ImageCompressor from "./components/ImageCompressor";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div >
+      <Navbar/>
+      <ImageCompressor/>
+      <br/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
